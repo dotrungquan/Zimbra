addd0ef8dd351461c69eada961dbe5b389e0ade6
dotrungquan <56627093+dotrungquan@users.noreply.github.com>
2022-12-12 15:43:52 +0700

    Update change-hostname.sh

--- change-hostname.sh
+++ change-hostname.sh
@@ -32,8 +32,8 @@ sed -i -e 's/'$oldhostname'/'$newhostname'/g' /opt/zimbra/conf/nginx/includes/ng
 su - zimbra -c  "zmloggerhostmap -d $oldhostname $oldhostname"
 su - zimbra -c  "zmloggerhostmap -d mail $oldhostname"
 
-sed -i -e 's/'$oldhostname'/'$newhostname'/g' /etc/hostname
-sed -i -e 's/'$oldhostname'/'$newhostname'/g' /etc/hosts
-sed -i -e 's/'$oldhostname'/'$newhostname'/g' /etc/dnsmasq.conf
+#sed -i -e 's/'$oldhostname'/'$newhostname'/g' /etc/hostname
+#sed -i -e 's/'$oldhostname'/'$newhostname'/g' /etc/hosts
+#sed -i -e 's/'$oldhostname'/'$newhostname'/g' /etc/dnsmasq.conf
 
 reboot
